cf6788bd2313efe56321fb2bbf54342b148700b1
Sergey <bivenmira1990@gmail.com>
2021-02-28 11:05:59 +0300

    Created a folder of my components and created a lot page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,10 @@
 <template>
-<div>
-<SEARCH/>
-<LOTS/>
-</div>
+
 </template>
 
 <script>
-import SEARCH from "@/components/SEARCH";
-import LOTS from "@/components/LOTS";
+
 export default {
 
-  components: {
-    SEARCH,
-    LOTS
-   }
 }
 </script>
