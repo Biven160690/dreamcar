51296755223bc4e4099917a36e3906429226a44d
Leonov Daniil <suppaherovodka@mail.ru>
2021-03-05 16:06:48 +0300

    Inin authorization logic
    All users from jsonplaceholder have password "12345"

---
+++ store/index.js
@@ -0,0 +1,15 @@
+//Главный store
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import users from './modules/users'
+
+Vue.use(Vuex)
+
+const store = () => new Vuex.Store({
+    modules: {
+        users,
+    }
+})
+
+export default store
